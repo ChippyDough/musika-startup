81af0e7487a9cfa90fcc381621ed089ee67dee5f
Musika <musika.startup@gmail.com>
2019-05-15 15:11:50 +0900

    Added upload image function in admin dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_054146) do
+ActiveRecord::Schema.define(version: 2019_05_15_060013) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -44,6 +44,10 @@ ActiveRecord::Schema.define(version: 2019_05_15_054146) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "tasks", force: :cascade do |t|
