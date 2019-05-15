dc864827c9caae93ea0b351622cca835c6d4d34f
Musika <musika.startup@gmail.com>
2019-05-15 15:53:46 +0900

    Pre styling courses page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+ 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
 
   root 'pages#home'
+
+  resources :project do
+  	resources :task, only: [:show]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
 end
